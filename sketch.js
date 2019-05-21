795c9156421195993753bfae407a27b57d1b6359
Trey-Wenrick <50748791+Trey-Wenrick@users.noreply.github.com>
2019-05-21 09:46:03 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,165 +1,162 @@
+var mainCharacter;
+var gravity = 9.8/30.0;
+var backgroundImage;
+var mainCharacterImage;
+var monsterImage
+var groundOffset = 100
+var monsterArray = []
+var health = 100
+var score = 0
 
-// the snake is divided into small segments, which are drawn and edited on each 'draw' call
-let numSegments = 10;
-let direction = 'right';
-
-const xStart = 0; //starting x coordinate for snake
-const yStart = 250; //starting y coordinate for snake
-const diff = 10;
-
-let xCor = [];
-let yCor = [];
-
-let xFruit = 0;
-let yFruit = 0;
-let scoreElem;
-
-function setup() {
-  scoreElem = createDiv('Score = 0');
-  scoreElem.position(20, 20);
-  scoreElem.id = 'score';
-  scoreElem.style('color', 'white');
-
-  createCanvas(500, 500);
-  frameRate(15);
-  stroke(255);
-  strokeWeight(10);
-  updateFruitCoordinates();
-
-  for (let i = 0; i < numSegments; i++) {
-    xCor.push(xStart + i * diff);
-    yCor.push(yStart);
+class Character {
+  constructor(x, y, width) {
+    this.x = x;
+    this.y = y;
+    this.ySpeed = 0;
+    this.xSpeed = 0;
+    this.width = width;
+    this.color = "blue"
+    this.isMonster = false
+    this.isDead = false
+    this.targetX = random()*width
   }
-}
-
-function draw() {
-  background(0);
-  for (let i = 0; i < numSegments - 1; i++) {
-    line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
+  
+  update(){
+	if(this.y+this.width*0.5 >= (height-groundOffset) && this.ySpeed > 0) 
+    {
+      this.ySpeed = this.ySpeed*(-0.4)
+      this.y = height-this.width*0.5-groundOffset
+    }
+   this.ySpeed += gravity;
+   this.y += this.ySpeed;
+    
+   this.xSpeed *= 0.8
+   this.x += this.xSpeed;
   }
-  updateSnakeCoordinates();
-  checkGameStatus();
-  checkForFruit();
-}
-
-/*
- The segments are updated based on the direction of the snake.
- All segments from 0 to n-1 are just copied over to 1 till n, i.e. segment 0
- gets the value of segment 1, segment 1 gets the value of segment 2, and so on,
- and this results in the movement of the snake.
-
- The last segment is added based on the direction in which the snake is going,
- if it's going left or right, the last segment's x coordinate is increased by a
- predefined value 'diff' than its second to last segment. And if it's going up
- or down, the segment's y coordinate is affected.
-*/
-function updateSnakeCoordinates() {
-  for (let i = 0; i < numSegments - 1; i++) {
-    xCor[i] = xCor[i + 1];
-    yCor[i] = yCor[i + 1];
+  
+  moveBadGuy(){
+    var differenceX = this.targetX-this.x
+    this.xSpeed += differenceX*0.002
+    
+    if(random() >= 0.98){
+      this.targetX = random()*width
+      this.ySpeed -= 10
+    }
+    
+    
+    this.isTouchingMainCharacter()
   }
-  switch (direction) {
-    case 'right':
-      xCor[numSegments - 1] = xCor[numSegments - 2] + diff;
-      yCor[numSegments - 1] = yCor[numSegments - 2];
-      break;
-    case 'up':
-      xCor[numSegments - 1] = xCor[numSegments - 2];
-      yCor[numSegments - 1] = yCor[numSegments - 2] - diff;
-      break;
-    case 'left':
-      xCor[numSegments - 1] = xCor[numSegments - 2] - diff;
-      yCor[numSegments - 1] = yCor[numSegments - 2];
-      break;
-    case 'down':
-      xCor[numSegments - 1] = xCor[numSegments - 2];
-      yCor[numSegments - 1] = yCor[numSegments - 2] + diff;
-      break;
+  
+  isTouchingMainCharacter(){
+    if(mainCharacter.x + mainCharacter.width >= this.x
+       && mainCharacter.x <= this.x+this.width
+       && mainCharacter.y + mainCharacter.width >= this.y
+       && mainCharacter.y <= this.y+this.width)
+    {
+      //here we have figured out that there was a hit
+      stroke("black")
+      noFill()
+      rect(this.x, this.y, this.width, this.width)
+      
+      if(mainCharacter.y - this.y < -30){
+        this.isDead = true
+        mainCharacter.ySpeed = -5
+        score += 2
+      } else {
+        health -= 1
+      }
+    }
   }
-}
-
-/*
- I always check the snake's head position xCor[xCor.length - 1] and
- yCor[yCor.length - 1] to see if it touches the game's boundaries
- or if the snake hits itself.
-*/
-function checkGameStatus() {
-  if (
-    xCor[xCor.length - 1] > width ||
-    xCor[xCor.length - 1] < 0 ||
-    yCor[yCor.length - 1] > height ||
-    yCor[yCor.length - 1] < 0 ||
-    checkSnakeCollision()
-  ) {
-    noLoop();
-    const scoreVal = parseInt(scoreElem.html().substring(8));
-    scoreElem.html('Game ended! Your score was : ' + scoreVal);
-  }
-}
-
-/*
- If the snake hits itself, that means the snake head's (x,y) coordinate
- has to be the same as one of its own segment's (x,y) coordinate.
-*/
-function checkSnakeCollision() {
-  const snakeHeadX = xCor[xCor.length - 1];
-  const snakeHeadY = yCor[yCor.length - 1];
-  for (let i = 0; i < xCor.length - 1; i++) {
-    if (xCor[i] === snakeHeadX && yCor[i] === snakeHeadY) {
-      return true;
+  
+  
+  draw(){
+    if(this.isMonster){
+      image(monsterImage, this.x, this.y, this.width, this.width)  
+    } else {
+      image(mainCharacterImage, this.x, this.y, this.width, this.width)
     }
   }
 }
 
-/*
- Whenever the snake consumes a fruit, I increment the number of segments,
- and just insert the tail segment again at the start of the array (basically
- I add the last segment again at the tail, thereby extending the tail)
-*/
-function checkForFruit() {
-  point(xFruit, yFruit);
-  if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
-    const prevScore = parseInt(scoreElem.html().substring(8));
-    scoreElem.html('Score = ' + (prevScore + 1));
-    xCor.unshift(xCor[0]);
-    yCor.unshift(yCor[0]);
-    numSegments++;
-    updateFruitCoordinates();
+function setup() {
+  createCanvas(800, 400);
+  mainCharacter = new Character(200, 400, 120)
+  backgroundImage = loadImage("./desert.jpg")
+  mainCharacterImage = loadImage("./d9pdvtm-20fd2599-dde8-44fd-a301-1b8b12829dbb.png")
+  monsterImage = loadImage("./goomba.png")
+  
+  for(var i = 0;i<100;i++){
+    var newMonster = new Character(500+20*i, 100, 60)
+    newMonster.isMonster = true
+    monsterArray.push(newMonster)
+  }
+
+}
+
+function draw() {
+  if(health <= 0){
+    background(0)
+    fill("blue")
+    text("GAME OVER. \nSCORE " + score, 200, 200, 200, 200)
+    return 
+  }
+  
+  
+  background(0, 200, 150);
+  image(backgroundImage, 0, 0, width, height)
+  
+  if(keyIsDown(LEFT_ARROW)){
+    //move left
+    mainCharacter.xSpeed -= 1.0
+  }
+  
+  if(keyIsDown(RIGHT_ARROW)){
+    //move right
+    mainCharacter.xSpeed += 1.0
+  }
+  
+  mainCharacter.update();
+  mainCharacter.draw()
+  
+  //draw health bar
+  fill("purple")
+  stroke("black")
+  rect(10, 10, health*2, 20)
+  
+  fill("white")
+  textSize(30)
+  text(score, width-40, 10, 40, 20)
+  
+  
+  var anyCatAlive = false
+  
+  for(var i = 0;i<monsterArray.length;i++)
+  {
+    if(monsterArray[i].isDead){
+      //do nothing! (because monster is dead)
+    } else { 
+      anyCatAlive = true
+      monsterArray[i].update()
+      monsterArray[i].moveBadGuy()
+      monsterArray[i].draw() 
+    }
+  }
+  
+  if(anyCatAlive === false){
+    for(var i = 0;i<10;i++){
+      var newMonster = new Character(500+20*i, 100+ 10, 60)
+      newMonster.isMonster = true
+      monsterArray.push(newMonster)
+    }
+  }
+  
+}
+
+function keyPressed(){
+   //&& mainCharacter.y >= 260
+  if(key === " "){
+    //JUMP!
+    mainCharacter.ySpeed -= 10.0
   }
 }
 
-function updateFruitCoordinates() {
-  /*
-    The complex math logic is because I wanted the point to lie
-    in between 100 and width-100, and be rounded off to the nearest
-    number divisible by 10, since I move the snake in multiples of 10.
-  */
-
-  xFruit = floor(random(10, (width - 100) / 10)) * 10;
-  yFruit = floor(random(10, (height - 100) / 10)) * 10;
-}
-
-function keyPressed() {
-  switch (keyCode) {
-    case 74:
-      if (direction !== 'right') {
-        direction = 'left';
-      }
-      break;
-    case 76:
-      if (direction !== 'left') {
-        direction = 'right';
-      }
-      break;
-    case 73:
-      if (direction !== 'down') {
-        direction = 'up';
-      }
-      break;
-    case 75:
-      if (direction !== 'up') {
-        direction = 'down';
-      }
-      break;
-  }
-}
