afbf8dee8a325cf1384099786468f727849f3bc1
Trey-Wenrick <50748791+Trey-Wenrick@users.noreply.github.com>
2019-05-28 10:10:19 -0700

    Add files via upload

--- ball.js
+++ ball.js
@@ -4,59 +4,57 @@ var x = 0
 var y = 0
 
 function setup() {
-  createCanvas(400, 400);
-  greeting = createElement('h2','What Color Bubble?');
-  input = createInput();
-  button = createButton('Submit');
-  button.mousePressed(greet);
-  
-  greeting2 = createElement('h2','What Color Background?');
-  backgroundcolor = createInput();
-  button2 = createButton('Submit');
-  button2.mousePressed(greet2);
+    createCanvas(400, 400);
+    greeting = createElement('h2','What Color Bubble?');
+    input = createInput();
+    button = createButton('Submit');
+    button.mousePressed(greet);
+
+    greeting2 = createElement('h2','What Color Background?');
+    backgroundcolor = createInput();
+    button2 = createButton('Submit');
+    button2.mousePressed(greet2);
 }
 
-function greet() { 
-  const color = input.value();
-  greeting.html('Nice ' + color + '!');
-  input.value(color);
-	
+function greet() {
+    const color = input.value();
+    greeting.html('Nice ' + color + '!');
+    input.value(color);
+
 }
 function greet2() {
-  const color2 = backgroundcolor.value();
-  greeting2.html('Nice ' + color2 + '!');
-  backgroundcolor.value(color2)
-	
+    const color2 = backgroundcolor.value();
+    greeting2.html('Nice ' + color2 + '!');
+    backgroundcolor.value(color2)
+
 }
 
 function draw() {
-  background(backgroundcolor.value());
-  stroke(0);
-  strokeWeight(4);
-  fill(input.value());
-  ellipse(x,200,75,75);
-  ellipse(200,y,75,75);
-  ellipse(x,y,75,75);
-  
-  
-  if(x > width) {
-    speed = -3;
-  }
-  
-  if (x < 0) {
-    speed = 3
-  }
-  
-  if(y > height) {
-    speed = -3;
-  }
-  
-  if (y < 0) {
-    speed = 3
-  }
-  
-  x = x + speed
-  y = y + speed
+    background(backgroundcolor.value());
+    stroke(0);
+    strokeWeight(4);
+    fill(input.value());
+    ellipse(x,200,75,75);
+    ellipse(200,y,75,75);
+    ellipse(x,y,75,75);
+
+
+    if(x > width) {
+        speed = -3;
+    }
+
+    if (x < 0) {
+        speed = 3
+    }
+
+    if(y > height) {
+        speed = -3;
+    }
+
+    if (y < 0) {
+        speed = 3
+    }
+
+    x = x + speed
+    y = y + speed
 }
-
-
--- clock.js
+++ clock.js
@@ -1,34 +1,34 @@
 function setup() {
-  createCanvas(400, 400);
-  angleMode(DEGREES);
+    createCanvas(400, 400);
+    angleMode(DEGREES);
 }
 
 function draw() {
-  background(0);
+    background(0);
 
-  let hr = hour();
-  let mn = minute();
-  let sc = second();
-  let dy = day();
-  
-  strokeWeight(0);
-  textSize(50);
-  textFont();
-  fill(255)
-  text(hr + ':' + mn + ':' + sc,110,210)
-  
-  translate(200,200);
-  rotate(-90);
-  strokeWeight(8 );
-  stroke(255,100,150);
-  noFill();
-  let end = map (sc,0,60,0,360);
-  arc(0,0,300,300,0,end);
-  stroke(100,150,255);
-  let end1 = map (mn,0,60,0,360);
-  arc(0,0,280,280,0,end1);
-  stroke(150,255,100);
-  let end2 = map (hr,0,12,0,360);
-  arc(0,0,260,260,0,end2);
-  
+    let hr = hour();
+    let mn = minute();
+    let sc = second();
+    let dy = day();
+
+    strokeWeight(0);
+    textSize(50);
+    textFont();
+    fill(255)
+    text(hr + ':' + mn + ':' + sc,110,210)
+
+    translate(200,200);
+    rotate(-90);
+    strokeWeight(8 );
+    stroke(255,100,150);
+    noFill();
+    let end = map (sc,0,60,0,360);
+    arc(0,0,300,300,0,end);
+    stroke(100,150,255);
+    let end1 = map (mn,0,60,0,360);
+    arc(0,0,280,280,0,end1);
+    stroke(150,255,100);
+    let end2 = map (hr,0,12,0,360);
+    arc(0,0,260,260,0,end2);
+
 }
